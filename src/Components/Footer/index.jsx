a8a7672b2ce744f1990d9e01f26d4e35cbd69e30
fiji <hiddeoutcollective@gmail.com>
2024-11-24 12:00:06 -0500

    fefe

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Footer extends React.Component {
   render() {
     return (
-      <footer>
+      <footer style={{ marginTop: '-20px' }}>  {/* Added negative margin */}
         <div className="copyright">Copyright &copy; 2024 - The Bastion Bot Project - Modified By The Marly Team</div>
       </footer>
     );
